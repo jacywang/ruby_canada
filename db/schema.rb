2833a0c8855a447e4694febdbfd2a19922014b87
Jacy Wang <jacywj@gmail.com>
2015-01-17 17:37:15 -0500

    Add User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150117152917) do
+ActiveRecord::Schema.define(version: 20150117223440) do
 
   create_table "topics", force: true do |t|
     t.string   "title"
     t.text     "content"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
