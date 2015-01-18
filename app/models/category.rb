ff6712cec0f50696cf18cd6571887b7cf0cf553e
Jacy Wang <jacywj@gmail.com>
2015-01-17 22:31:15 -0500

    Add CategoriesController

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :topic_categories
   has_many :topics, through: :topic_categories
+
+  validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 PostitTemplate::Application.routes.draw do
   root to: 'topics#index'
   resources :topics, only: [:index, :show, :new, :create, :edit, :update]
+  resources :categories, only: [:new, :create, :show]
 end
